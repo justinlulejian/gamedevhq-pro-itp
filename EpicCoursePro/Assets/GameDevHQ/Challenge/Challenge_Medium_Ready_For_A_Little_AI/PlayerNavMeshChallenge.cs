2cf1e8bb23a343b37847a4973d35ea74a541df21
Justin Lulejian <justin@lulejian.com>
2021-02-15 15:06:23 -0800

    initial commit having done nav challenge, haven't done anything with epic project yet.

---
+++ EpicCoursePro/Assets/GameDevHQ/Challenge/Challenge_Medium_Ready_For_A_Little_AI/PlayerNavMeshChallenge.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerNavMeshChallenge : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _target;
+    private NavMeshAgent _navMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _navMeshAgent.SetDestination(_target.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
