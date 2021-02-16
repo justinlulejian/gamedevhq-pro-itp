da49d6670b376bdf44ec809f0752ea1eb0dbbcf2
Justin L <justinlulejian@users.noreply.github.com>
2021-02-16 13:01:32 -0800

    AI Navigation Feature
    Components: 
    * Bake Navmesh Floor
    * AI Should only walk the baked path
    * AI should walk 1.5 meters per second (adjustable in the inspector) 
    * Walk Animations Playing by Default

---
+++ EpicCoursePro/Assets/GameDevHQ/Scripts/MechOne.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class MechOne : MonoBehaviour
+{
+    [SerializeField] private GameObject _navTarget;
+    
+    private NavMeshAgent _navMeshAgent;
+    
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _navMeshAgent.SetDestination(_navTarget.transform.position);
+    }
+}
