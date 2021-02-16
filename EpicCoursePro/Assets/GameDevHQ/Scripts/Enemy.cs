29cb05d89da8568e43c41f9a9593191b9d4e91a1
Justin Lulejian <justin@lulejian.com>
2021-02-16 14:11:02 -0800

    AI: AI have warFund (currency) value for being killed

--- EpicCoursePro/Assets/GameDevHQ/Scripts/Enemy.cs
+++ EpicCoursePro/Assets/GameDevHQ/Scripts/Enemy.cs
@@ -5,23 +5,44 @@ namespace GameDevHQ.Scripts
 {
     public class Enemy : MonoBehaviour
     {
-        [SerializeField] 
+        [SerializeField] [Tooltip("The end destination the enemy will attempt to navigate to.")][Header("Navigation components")]
         private GameObject _navTarget;
+        private NavMeshAgent _navMeshAgent;
 
-        [SerializeField] 
+        [SerializeField] [Header("Health Settings")]
         private int _health = 100;
 
-        private NavMeshAgent _navMeshAgent;
+        [Header("Currency Settings")]
+        [SerializeField] [Tooltip("The currency value provided when the enemy is killed by player.")]
+        private int warFundValue = 100;
+       
         void Start()
         {
             _navMeshAgent = GetComponent<NavMeshAgent>();
+            
+            if (_navMeshAgent == null)
+            {
+                Debug.LogError($"Nav mesh agent is null on enemy: {this.gameObject.name}");
+            }
+            
             _navMeshAgent.SetDestination(_navTarget.transform.position);
-
         }
-
+        
         public void Damage(int damageValue)
         {
             Mathf.Min(0, _health -= damageValue);
+
+            if (_health <= 0)
+            {
+                Kill();
+            }
         }
+        
+        private void Kill()
+        {
+            Destroy(this.gameObject);
+        }
+        
+        
     }
 }
