395bd4c4df9211f76c99eb3c85c3bb7246b6e923
Justin L <justinlulejian@users.noreply.github.com>
2021-02-16 13:09:41 -0800

    AI: Modular class to support multiple enemy types

---
+++ EpicCoursePro/Assets/GameDevHQ/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace GameDevHQ.Scripts
+{
+    public class Enemy : MonoBehaviour
+    {
+        [SerializeField] 
+        private GameObject _navTarget;
+
+        private NavMeshAgent _navMeshAgent;
+        // Start is called before the first frame update
+        void Start()
+        {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+            _navMeshAgent.SetDestination(_navTarget.transform.position);
+
+        }
+    
+    }
+}
