06df98995a11c9dc68d050cdbb86e9c681df6d3c
Justin Lulejian <justin@lulejian.com>
2021-02-15 16:06:28 -0800

    got a single mech to nav through map, but needs a bit of adjustment with bake settings

---
+++ EpicCoursePro/Assets/MechOne.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MechOne : MonoBehaviour
+{
+    [SerializeField] private GameObject _navTarget;
+    
+    private NavMeshAgent _navMeshAgent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _navMeshAgent.SetDestination(_navTarget.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
