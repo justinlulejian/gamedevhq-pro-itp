519cde2aaba41193511f1146b0edf6845b0a76b1
Justin Lulejian <justin@lulejian.com>
2021-02-16 12:56:09 -0800

    Navmesh is baked, characters only use it for movement, and they are set to walk at 1.5 meters.

--- EpicCoursePro/Assets/GameDevHQ/Scripts/MechOne.cs
+++ EpicCoursePro/Assets/GameDevHQ/Scripts/MechOne.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
 public class MechOne : MonoBehaviour
@@ -9,16 +7,9 @@ public class MechOne : MonoBehaviour
     
     private NavMeshAgent _navMeshAgent;
     
-    // Start is called before the first frame update
     void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
         _navMeshAgent.SetDestination(_navTarget.transform.position);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
