adbf922221e25ada649d9cbe5f9bb7b93c17bd05
Justin Lulejian <justin@lulejian.com>
2021-02-16 13:28:05 -0800

    AI has health properties

--- EpicCoursePro/Assets/GameDevHQ/Scripts/Enemy.cs
+++ EpicCoursePro/Assets/GameDevHQ/Scripts/Enemy.cs
@@ -8,6 +8,9 @@ namespace GameDevHQ.Scripts
         [SerializeField] 
         private GameObject _navTarget;
 
+        [SerializeField] 
+        private int _health = 100;
+
         private NavMeshAgent _navMeshAgent;
         // Start is called before the first frame update
         void Start()
@@ -16,6 +19,10 @@ namespace GameDevHQ.Scripts
             _navMeshAgent.SetDestination(_navTarget.transform.position);
 
         }
-    
+
+        public void Damage(int damageValue)
+        {
+            Mathf.Min(0, _health -= damageValue);
+        }
     }
 }
