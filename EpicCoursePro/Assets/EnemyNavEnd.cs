5a9fdd1850e8e54a8923817b8ca28c05439b4439
Justin L <justinlulejian@users.noreply.github.com>
2021-02-16 19:56:40 -0800

    Spawn Manager Basic Spawning Features
    * Create start & end points on the map
    * Turn the SpawnManager into a Singleton
    * Spawn Enemies at the start position

---
+++ EpicCoursePro/Assets/EnemyNavEnd.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class EnemyNavEnd : MonoBehaviour
+{
+    public static Action<GameObject> EnemyCollision;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            EnemyCollision.Invoke(other.gameObject);
+        }
+    }
+}
