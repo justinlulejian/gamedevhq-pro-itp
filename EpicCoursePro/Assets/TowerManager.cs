8ab450244b4c78d2c593547ed8ccbd4aee870440
Justin Lulejian <justin@lulejian.com>
2021-02-26 16:55:58 -0800

    Towers: Add tower spots with manager and manually controlled available particle effect.

---
+++ EpicCoursePro/Assets/TowerManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class TowerManager : MonoSingleton<TowerManager>
+{
+    [SerializeField] 
+    private GameObject _towerSpotContainer;
+
+    public static event Action onActivateTowers;
+    public static event Action onDeactivateTowers;
+
+    private void AddTowerToContainer(TowerSpot spot)
+    {
+        spot.transform.parent = _towerSpotContainer.transform;
+    }
+
+    private void Update()
+    {
+        // TODO: Test code to test tower particle system activation.
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            onActivateTowers.Invoke();
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            onDeactivateTowers.Invoke();
+        }
+    }
+}
