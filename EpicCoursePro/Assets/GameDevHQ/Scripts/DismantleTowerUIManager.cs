cded034f9e99edcf54529bdc089c9d568ad1f833
Justin Lulejian <justin@lulejian.com>
2021-03-12 15:30:53 -0800

    Upgrade UI: Completed upgrade and dismantle UI with upgraded towers.

--- EpicCoursePro/Assets/GameDevHQ/Scripts/DismantleTowerUIManager.cs
+++ EpicCoursePro/Assets/GameDevHQ/Scripts/DismantleTowerUIManager.cs
@@ -1,112 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace GameDevHQ.Scripts
 {
     // TODO: refactor this to have a shared AbstractUIManager class with UpgradeGunUIManager.
-    public class DismantleTowerUIManager : MonoSingleton<DismantleTowerUIManager>
+    public class DismantleTowerUIManager : AbstractTowerUIManager
     {
-        [SerializeField] 
-        private Button _confirmButton;
-        [SerializeField] 
-        private Button _cancelButton;
-        [SerializeField]
-        private Text _dismantleValueText;
-        
-        private List<Text> _texts = new List<Text>();
-        private List<Button> _buttons = new List<Button>();
-        private List<Image> _images = new List<Image>();
-        private TowerSpot _towerSpotUpgrade;
-        private Image _image;
-
         public static event Action<TowerSpot> onDismantleTower;
         
-        protected override void Awake()
-            {
-                base.Awake();
-                _image = GetComponent<Image>();
-                _texts = GetComponentsInChildren<Text>().ToList();
-                _buttons.Add(_confirmButton);
-                _buttons.Add(_cancelButton);
-                _images.Add(_image);
-                _images.AddRange(GetComponentsInChildren<Image>());
-        
-                if (_image == null)
-                {
-                    Debug.LogError($"Dismantle UI doesn't have access to Image component.");
-                }
-                if (_texts.Count < 1)
-                {
-                    Debug.LogError($"Dismantle UI doesn't have access to text child components.");
-                }
-                if (_buttons.Count < 1)
-                {
-                    Debug.LogError($"Dismantle UI doesn't have access to button child components.");
-                }
-                if (_images.Count < 2)
-                {
-                    Debug.LogError($"Dismantle UI doesn't have access to all image/button child" +
-                                   $" components.");
-                }
-                if (_confirmButton == null)
-                {
-                    Debug.LogError($"Dismantle UI doesn't have access to confirm button.");
-                }
-                if (_cancelButton == null)
-                {
-                    Debug.LogError($"Dismantle UI doesn't have access to cancel button.");
-                }
-            }
-    
-        private void EnableDisableUI(bool onoff)
+        protected override void PresentUI()
         {
-            foreach (var text in _texts)
-            {
-                text.enabled = onoff;
-            }
-            foreach (var button in _buttons)
-            {
-                button.enabled = onoff;
-            }
-            foreach (var image in _images)
-            {
-                image.enabled = onoff;
-            }
-        }
-
-        private void TurnOnUI()
-        {
-            EnableDisableUI(true);
-        }
-
-        public void TurnOffUI()
-        {
-            EnableDisableUI(false);
-            _towerSpotUpgrade = null;
-        }
-        
-        public void PresentDismantleUI(TowerSpot towerSpot)
-        {
-            Debug.Log("Presenting dismantling UI.");
-            _dismantleValueText.text = towerSpot.GetTowerPlacedOnSpot().WarFundValue.ToString();
-            _towerSpotUpgrade = towerSpot;
-            TurnOnUI();
+            _warFundsValueText.text = Spot.GetTowerPlacedOnSpot().WarFundValue.ToString();
         }
 
         public void DismantleTower()
         {
-            Debug.Log("Dismantling tower.");
-            Tower tower = _towerSpotUpgrade.GetTowerPlacedOnSpot();
+            Tower tower = Spot.GetTowerPlacedOnSpot();
             // TODO: create a new property that stores the dismantle value.
             GameManager.Instance.AddWarFunds(tower.WarFundValue);
-            onDismantleTower?.Invoke(_towerSpotUpgrade);
-            // TODO: once upgrade towers are pooled switch to pooling.
-            Destroy(tower.gameObject);
-            // PoolManager.Instance.RecyclePooledObj(tower.gameObject);
-            _towerSpotUpgrade = null;
+            onDismantleTower?.Invoke(Spot);
+            Spot = null;
+            TurnOffUI();
         }
     }
 }
---
+++ EpicCoursePro/Assets/GameDevHQ/Scripts/PlayerUIManager.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GameDevHQ.Scripts;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PlayerUIManager : MonoSingleton<PlayerUIManager>
+{
+    [SerializeField] 
+    private GameObject _warFundsUIObject;
+    private WarFundsUIManager _warFundsUI;
+    [SerializeField] 
+    private List<GameObject> _upgradeTowerUIObjects = new List<GameObject>();
+    private List<UpgradeGunUIManager> _upgradeGunUIs;
+    [SerializeField] 
+    private GameObject _dismantleUIObject;
+    private DismantleTowerUIManager _dismantleUI;
+
+    private List<GameObject> _interactableUIObjects = new List<GameObject>();
+
+    public enum UIStates
+    {
+        UpgradeGatlingTowerUIState,
+        UpgradeMissileTowerUIState,
+        DismantleTowerUIState,
+    }
+
+    private Dictionary<UIStates, AbstractTowerUIManager> _uiStateToUIMap =
+        new Dictionary<UIStates, AbstractTowerUIManager>();
+    
+    private void OnEnable()
+    {
+        TowerSpot.onUserTowerInteractIntent += EnableTowerUI;
+    }
+
+    private void OnDisable()
+    {
+        TowerSpot.onUserTowerInteractIntent -= EnableTowerUI;
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _warFundsUI = _warFundsUIObject.GetComponent<WarFundsUIManager>();
+        foreach (var upgradeTowerUIObject in _upgradeTowerUIObjects)
+        {
+            UpgradeGunUIManager towerUpgradeUI = (
+                upgradeTowerUIObject.GetComponent<UpgradeGunUIManager>());
+            // TODO: Change this to be dynamic check for different tower types.
+            if (towerUpgradeUI.TowerToUpgrade.TowerType == 1)
+            {
+                _uiStateToUIMap[UIStates.UpgradeGatlingTowerUIState] = towerUpgradeUI;
+            } else if (towerUpgradeUI.TowerToUpgrade.TowerType == 2)
+            {
+                _uiStateToUIMap[UIStates.UpgradeMissileTowerUIState] = towerUpgradeUI;
+            }
+            _interactableUIObjects.Add(upgradeTowerUIObject);
+        }
+        _dismantleUI = _dismantleUIObject.GetComponent<DismantleTowerUIManager>();
+        _uiStateToUIMap[UIStates.DismantleTowerUIState] = _dismantleUI;
+
+        if (_warFundsUI == null) 
+        {
+            Debug.LogError("UI Manager does not have reference to war funds UI behavior " +
+                           "script.");
+        }
+        if (_upgradeTowerUIObjects.Count < 1) 
+        {
+            Debug.LogError("UI Manager does not have reference to upgrade UI behavior " +
+                           "scripts.");
+        }
+        if (_dismantleUI == null) 
+        {
+            Debug.LogError("UI Manager does not have reference to dismantle tower UI " +
+                           "behavior script.");
+        }
+    }
+
+    private void EnableTowerUI(UIStates uiState, TowerSpot towerSpot)
+    {
+        if (_interactableUIObjects.Any(ui => ui.activeSelf))
+        {
+            // UIs should not override one another. They should be dismissed by player.
+            return;
+        }
+        
+        // TODO(checkpoint): How will I communicate the tower spot to the 
+        AbstractTowerUIManager uiManager = _uiStateToUIMap[uiState];
+        uiManager.Spot = towerSpot;
+        uiManager.gameObject.SetActive(true);
+    }
+    
+    private void DisableTowerUI(UIStates uiState)
+    {
+        
+        _uiStateToUIMap[uiState].gameObject.SetActive(false);
+    }
+    
+    private void DisableAllUI()
+    {
+        _interactableUIObjects.ForEach(ui => ui.gameObject.SetActive(false));
+    }
+
+}
