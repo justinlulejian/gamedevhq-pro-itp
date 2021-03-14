aacd32ad41166792c41bfa60524e56c2a4ef5f8a
Justin Lulejian <justin@lulejian.com>
2021-03-14 15:22:04 -0700

    Dissolve shader: implemented on death for both mechs.

--- EpicCoursePro/Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/3D/Characters/Robots/Mech_02/Script/Parent_Turret_Y.cs
+++ EpicCoursePro/Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/3D/Characters/Robots/Mech_02/Script/Parent_Turret_Y.cs
@@ -1,19 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Parent_Turret_Y : MonoBehaviour
 {
-
     public GameObject Parent_Point;
+    
+    [SerializeField]
+    private List<MeshRenderer> _dissolveMeshRenderers = new List<MeshRenderer>();
 
 
-
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-
+        _dissolveMeshRenderers = GetComponentsInChildren<MeshRenderer>().ToList();
+        
+        if (_dissolveMeshRenderers.Count < 1)
+        {
+            Debug.LogError($"Mesh renderer from parent turret script is non-existent on" +
+                           $" enemy {name}");
+        }
     }
+
+    public List<MeshRenderer> GetDissolveMeshRenderers()
+    {
+        return _dissolveMeshRenderers;
+    }
+
 
     // Update is called once per frame
     void LateUpdate()
